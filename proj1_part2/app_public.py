474fbe44cd512aa9659074b389eeee53a80fe7c8
Siddhesh Mhatre <sid1793@gmail.com>
2016-03-30 00:33:58 -0400

    wordcloud generated based on lat and long

--- proj1_part2/app_public.py
+++ proj1_part2/app_public.py
@@ -12,22 +12,14 @@ import re
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-# url_for('static', filename='style.css')
-# url_for('static', filename='style.css')
-# url_for('static', filename='style.css')
+
 try:
 	conn = psycopg2.connect("dbname='sm4083' user='sm4083' host='w4111db.eastus.cloudapp.azure.com' password='RPTDAA'")
-	# ////psql -h 128.59.17.200 -U keying -d lsde
-
 	conn.autocommit= True
 except Exception, e:
 	print e, "Connection Unsucessful"
 cur = conn.cursor()
 
-
-"""
-app.add_url_rule('/favicon.ico', redirect_to=url_for('static', filename='favicon.ico')) #url_for not defined
-"""
 
 @app.route('/',methods=["GET","POST"])
 def homepage():
@@ -39,7 +31,7 @@ def homepage():
 	data = cur.fetchall()
 	print data[0]
 	return render_template("index.html", cam_data = json.dumps(data))
-	#return render_template("index.html")
+	
 @app.route("/about.html")
 def about():
 	return render_template("about.html")
@@ -59,48 +51,30 @@ def images():
 	endtime = args.get('endtime')
 	camname = args.get('camname')
 	querystring = request.query_string
-	#datetime.strptime("2015-12-07 00:01:02", "%Y-%m-%d %H:%M:%S") #this should be put to the frontend
 	qry = "SELECT content from images WHERE camname=\'" +str(camname)+ "\' and time> \'"+ str(starttime) +"\' and time < \'"+ str(endtime)+"\';"
-	#print qry
+	
 	try:
 		cur.execute(qry)
 	except Exception,e:
 		print e
 		return "cannot find image"
-	#print args
+	
 	row = cur.fetchone()
-	#print type(row)
-	#print row
 	try:
 		bindata = base64.b64encode(row[0]).decode('utf-8')
 	except Exception, e:
 		print e
 		return "error using base64"
-	#print "returning bindata"
 	return bindata
 @app.route("/alerts")
 def alerts():
 	args = request.args
-#	istype = args.get('istype')
 	typename = args.get('typename')
 	startD = args.get('startD')
 	endD = args.get('endD')
 	startT = args.get('startT')
 	endT = args.get('endT')
-	# startD = start[0:9]
-	# endD = end[0:9]
-	# startT = start[11:]
-	# endT = end[11:]
-
-	# print 'start '+start
-	# print 'end '+end
-	#print 'startD '+startD
-	#print 'endD '+endD
-	#print 'startT '+startT
-	#print 'endT '+endT
-	#select '12/7/2015, 12:00:00 PM'-date_trunc('day', timestamp '12/7/2015, 12:00:00 PM') as timeonly;
-#	if istype == 'true':
-
+	
 	try:
 		cur.execute("SELECT location_lat, location_long from alerts WHERE type= %s \
 					 AND time::date BETWEEN %s and %s \
@@ -112,17 +86,8 @@ def alerts():
 		return "cannot find alerts"
 	
 	row = cur.fetchall()
-#		print "type of row: "+ str(type(row)) + "type of row[0]" + str(type(row[0])) + "type of row[0][0]" + str(type(row[0][0]))
-#		pprint(row)
 	return jsonify(coords = row)
-	#json.dumps(row)
-	
-	# "type: "+istype + ' typename: '+typename
 
-	# qry =	"SELECT location_lat,location_long from alerts WHERE type=\'" +str(typename)+ "\' and date_trunc('day', timestamp time)> \'"+ str(startD) +"\' and date_trunc('day', timestamp time) < \'"+ str(endD)+"\';"
-	#try 
-	#"SELECT time from alerts WHERE type=\'" +str(typename)+ "\' and date_trunc('day', timestamp time)> \'12/7/2015\' and date_trunc('day', timestamp time) < \'12/8/2015\';"
-	# SELECT time from alerts A WHERE type='hazard' and date_trunc('day', timestamp A.time) > '12/7/2015' and date_trunc('day', timestamp A.time) < '12/8/2015';
 @app.route('/wordCloud',methods=['POST'])
 def wordCloud():
 	data = json.loads(request.form['coord'])
@@ -130,7 +95,9 @@ def wordCloud():
 	sD = request.form['startDate']
 	eT = request.form['endTime']
 	eD = request.form['endDate']
-	qry ="SELECT content from tweets WHERE (lat BETWEEN "+ str(data[3]['lat']) +" and "+ str(data[1]['lat']) +");"
+	#qry ="SELECT content from tweets WHERE lat BETWEEN %s and %s and long BETWEEN %s and %s and time::date BETWEEN %s and %s and time::time BETWEEN %s and %s;" %(str(data[3]['lat']), str(data[1]['lat']),str(data[1]['lng']), str(data[3]['lng']),sD,eD,sT,eT) 
+	qry ="SELECT content from tweets WHERE lat BETWEEN %s and %s and long BETWEEN %s and %s" %(str(data[3]['lat']), str(data[1]['lat']),str(data[1]['lng']), str(data[3]['lng']))
+
 	print 'hello'
 	try:
 		cur.execute(qry)
